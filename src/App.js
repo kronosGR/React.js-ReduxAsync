cf34a931652c2f975d3196865fcdf790ab897f63
Georgios Katsanakis <geo.elgeo@gmail.com>
2021-12-07 10:16:17 +0100

    added store cart to firebase

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useEffect } from 'react';
 import { useSelector } from 'react-redux';
 
 import Cart from './components/Cart/Cart';
@@ -5,11 +6,20 @@ import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
 
 function App() {
-  const showCart =  useSelector((state) => state.ui.cartIsVisible)
+  const showCart = useSelector((state) => state.ui.cartIsVisible);
+  const cart = useSelector(state => state.cart);
+
+  useEffect(() => {
+    fetch('https://foodorder-cf025-default-rtdb.firebaseio.com/cart.json', {
+      method: 'PUT',
+      body: JSON.stringify(cart)
+    });
+  }, [ cart ]);
+
   return (
     <Layout>
-      {showCart && <Cart />}
-      <Products />
+      {showCart && <Cart/>}
+      <Products/>
     </Layout>
   );
 }
