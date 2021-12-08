8dea004a5fdeaf78e3c3657895cf7169934cb91b
Georgios Katsanakis <geo.elgeo@gmail.com>
2021-12-08 10:36:49 +0100

    added action for fetching cart from firebase

---
+++ src/store/cart-actions.js
@@ -0,0 +1,64 @@
+import { uiActions } from './ui-slice';
+import { cartActions } from './cart-slice';
+
+export const fetchCartData = () => {
+  return async (dispatch) => {
+    const fetchData = async () => {
+      const response = await fetch(
+        'https://foodorder-cf025-default-rtdb.firebaseio.com/cart.json');
+
+      if (!response.ok) {
+        throw new Error('Could not fetch the cart data');
+      }
+
+      const data = await response.json();
+      return data;
+    };
+    try {
+      const cartData = await fetchData();
+      dispatch(cartActions.replaceCart(cartData));
+    } catch (error) {
+      dispatch(uiActions.showNotification({
+        status: 'error',
+        title: 'Error',
+        message: 'Fetching cart data failed!'
+      }));
+    }
+  };
+};
+
+export const sendCartData = (cart) => {
+  return async (dispatch) => {
+    dispatch(uiActions.showNotification({
+      status: 'pending',
+      title: 'Sending...',
+      message: 'Sending cart data!'
+    }));
+
+    const sendRequest = async () => {
+      const response = await fetch(
+        'https://foodorder-cf025-default-rtdb.firebaseio.com/cart.json', {
+          method: 'PUT',
+          body: JSON.stringify(cart)
+        });
+
+      if (!response.ok) {
+        throw new Error('Sendind car data failed');
+      }
+    };
+    try {
+      await sendRequest();
+      dispatch(uiActions.showNotification({
+        status: 'success',
+        title: 'Success',
+        message: 'Send cart data successfully!'
+      }));
+    } catch (error) {
+      dispatch(uiActions.showNotification({
+        status: 'error',
+        title: 'Error',
+        message: 'Send cart data failed!'
+      }));
+    }
+  };
+};
